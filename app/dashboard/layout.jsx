358155ea528afd9e8c7faab446b881b4b622f6ce
Eduardo Retamales <eduardoretamalesrojas@gmail.com>
2024-03-04 16:18:47 -0300

    add styles dashboard

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,17 +1,18 @@
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import Navbar from "../ui/dashboard/navbar/navbar"
+import styles from "../ui/dashboard/dashboard.module.css"
 
 const Layout = ({children}) => {
     return (
-      <div>
-        <div>
-            <Sidebar />
-        </div>
-        <div>
-            <Navbar />
-            {children}
-        </div>
+      <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar/>
       </div>
+      <div className={styles.content}>
+        <Navbar/>
+        {children}
+      </div>
+    </div>
     )
   }
   
